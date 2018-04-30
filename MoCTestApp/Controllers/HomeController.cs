614938a0e6e781c23147c8d56f84c09b6a6f10f3
Richard Zach Wright <zachw@marketofchoice.com>
2018-04-30 12:27:54 -0700

    init commit

---
+++ MoCTestApp/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MoCTestApp.Models;
+using MoCTestApp.ViewModels;
+
+namespace MoCTestApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var viewModel = new ViewModels.IndexViewModel
+            {
+                CustomerList = Helpers.Customers.createCustomers("customer")
+            };
+
+            return View();
+        }
+
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ MoCTestApp/Helpers/Customers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoCTestApp.Helpers
+{
+    public class Customers
+    {
+        public static List<Customer> createCustomers()
+        {
+            List<Customer> customerList = new List<Customer>();
+
+            customerList.Add(new Customer(1, "Joe"));
+            customerList.Add(new Customer(2, "Sara"));
+            customerList.Add(new Customer(3, "John"));
+            customerList.Add(new Customer(4, "Melissa"));
+
+            return customerList;
+        }
+    }
+}
